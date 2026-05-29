cf76b396a24cbdac38ea43750e5232932e387945
sadaf953 <sadafmahvishedu@gmail.com>
2026-05-29 23:34:36 +0530

    fixed crm logic and styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // Add this
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Wrap App in HashRouter */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: '/business-crm/', // REPLACE 'business-crm' with your actual repo name
 })
